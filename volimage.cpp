9030a9941905c8b77e58c985442177038f526880
wylieza <wylieza@gmail.com>
2020-02-20 20:41:35 +0200

    Args handler now proccesses the args into c++ strings

--- volimage.cpp
+++ volimage.cpp
@@ -14,9 +14,13 @@ namespace WYLJUS002{
 int main(int argc, char* argv[]){
 
     //Args handler
-    std::string args(*argv);
-    std::cout << "There are " << argc << " arguments\n";
-    std::cout << args;
+    std::cout << "There are " << argc << " arguments\nArgs: ";
+
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; i ++){
+        args.push_back(std::string(argv[i]));
+        std::cout << args[i-1] << " ";
+    }
 
 
 }
